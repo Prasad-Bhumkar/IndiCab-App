b3b8982e2f19ab7b210518544dfb4c92785c283a
CodeSandbox <support@codesandbox.io>
2025-03-14 13:07:27 +0000

    next

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,11 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-    id("dagger.hilt.android.plugin") version "2.44" apply false // Hilt plugin
+    kotlin("jvm") version "1.9.0"
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+repositories {
+    mavenCentral()
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+dependencies {
+    // Add your dependencies here
 }
